87d3b5ee43421d2b7cc4a40ac742b2dee0967f31
r00j3K <adrianr8@o2.pl>
2026-04-22 19:06:32 +0200

    First commit

---
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/SimplifiedStockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.r00j3k.simplified_stock_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedStockMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedStockMarketApplication.class, args);
+	}
+
+}
---
+++ simplified_stock_market/src/test/java/com/github/r00j3k/simplified_stock_market/SimplifiedStockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.r00j3k.simplified_stock_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedStockMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
