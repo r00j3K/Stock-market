8adb37437737ea05ca3d189a8ef755f44835e46f
r00j3K <adrianr8@o2.pl>
2026-04-24 13:10:13 +0200

    Change naming conventions and field types, add constraints to certain tables

--- simplified_stock_market/src/main/resources/db/migration/V1.0.1__init_schema.sql
+++ simplified_stock_market/src/main/resources/db/migration/V1.0.1__init_schema.sql
@@ -1,33 +1,34 @@
 CREATE TYPE transaction_type AS ENUM ('buy', 'sell');
 
-CREATE TABLE wallet 
+CREATE TABLE wallets 
 (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+    wallet_id VARCHAR(255) PRIMARY KEY
 );
 
-CREATE TABLE bank 
+CREATE TABLE bank_stocks 
 (
     stock_name VARCHAR(255) PRIMARY KEY,
     quantity BIGINT NOT NULL DEFAULT 0
 );
 
-CREATE TABLE wallet_stock 
+CREATE TABLE wallet_stocks
 (
-    wallet_id BIGINT NOT NULL,
+    wallet_id VARCHAR(255) NOT NULL,
     stock_name VARCHAR(255) NOT NULL,
     quantity BIGINT NOT NULL DEFAULT 1,
     PRIMARY KEY (wallet_id, stock_name),
-    CONSTRAINT fk_wallet_id_wallet_stock FOREIGN KEY(wallet_id) REFERENCES wallet(id),
-    CONSTRAINT fk_stock_name FOREIGN KEY(stock_name) REFERENCES bank(stock_name)
+    CONSTRAINT fk_wallet_id_wallet_stock FOREIGN KEY(wallet_id) REFERENCES wallets(wallet_id),
+    CONSTRAINT fk_stock_name_wallet_stock FOREIGN KEY(stock_name) REFERENCES bank_stocks(stock_name)
 );
 
-CREATE TABLE audit_log 
+CREATE TABLE audit_logs 
 (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    wallet_id BIGINT NOT NULL,
+    log_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    wallet_id VARCHAR(255) NOT NULL,
     stock_name VARCHAR(255) NOT NULL,
-    type transaction_type NOT NULL,
+    transaction_type transaction_type NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_wallet_id_audit_log FOREIGN KEY(wallet_id) REFERENCES wallet(id)
+    CONSTRAINT fk_wallet_id_audit_log FOREIGN KEY(wallet_id) REFERENCES wallets(wallet_id),
+    CONSTRAINT fk_stock_name_audit_log FOREIGN KEY(stock_name) REFERENCES bank_stocks(stock_name)
 );
 
