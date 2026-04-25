de72d7ac61b7d4d28e0346e600a1141fc7c3c468
r00j3K <adrianr8@o2.pl>
2026-04-25 15:59:27 +0200

    Minor refactor

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/WalletStockRepository.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/WalletStockRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.r00j3k.simplified_stock_market.entities.WalletStock;
 import com.github.r00j3k.simplified_stock_market.entities.WalletStockId;
 
-public interface WalletStockRepository extends JpaRepository<WalletStock,WalletStockId> {
+public interface WalletStockRepository extends JpaRepository<WalletStock, WalletStockId> {
     List<WalletStock> findAllByWalletWalletId(String walletId);
 }
