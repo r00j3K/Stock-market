ffbe62e9ff02eb36d166e78129d978dcb93f888e
r00j3K <adrianr8@o2.pl>
2026-04-25 16:01:28 +0200

    Correct generic types order in AuditLogRepository

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/AuditLogRepository.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/AuditLogRepository.java
@@ -4,6 +4,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.r00j3k.simplified_stock_market.entities.AuditLog;
 
-public interface AuditLogRepository extends JpaRepository<Long, AuditLog> {
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
     List<AuditLog> findTop10000ByOrderByLogIdAsc();
 }
