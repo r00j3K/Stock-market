e69280aaad245764bda428e03d2079c2a8e02d19
r00j3K <adrianr8@o2.pl>
2026-04-25 19:05:23 +0200

    Add pessimistic locking

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/BankStockRepository.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/BankStockRepository.java
@@ -1,8 +1,15 @@
 package com.github.r00j3k.simplified_stock_market.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
 import com.github.r00j3k.simplified_stock_market.entities.BankStock;
 
+import jakarta.persistence.LockModeType;
+
 public interface BankStockRepository extends JpaRepository<BankStock, String> {
-    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<BankStock> findByIdForUpdate(String stockName);
 }
--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/WalletStockRepository.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/WalletStockRepository.java
@@ -1,10 +1,19 @@
 package com.github.r00j3k.simplified_stock_market.repositories;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import com.github.r00j3k.simplified_stock_market.entities.BankStock;
 import com.github.r00j3k.simplified_stock_market.entities.WalletStock;
 import com.github.r00j3k.simplified_stock_market.entities.WalletStockId;
 
+import jakarta.persistence.LockModeType;
+
 public interface WalletStockRepository extends JpaRepository<WalletStock, WalletStockId> {
     List<WalletStock> findAllByWalletWalletId(String walletId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<WalletStock> findByIdForUpdate(WalletStockId walletStockId);
 }
