26e044ff0fd30c7e838bfc111ae122041fb08c58
r00j3K <adrianr8@o2.pl>
2026-04-26 17:51:17 +0200

    Add validation rules for Dto fields

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/BankStocksDto.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/BankStocksDto.java
@@ -2,7 +2,12 @@ package com.github.r00j3k.simplified_stock_market.dtos;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 public record BankStocksDto(
+    @NotNull
+    @Valid
     List<StockListDto> stocks
 ) {}
 
--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/StockListDto.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/StockListDto.java
@@ -1,6 +1,16 @@
 package com.github.r00j3k.simplified_stock_market.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 public record StockListDto(
+    @NotBlank
+    @Size(max = 255)
     String name,
+
+    @NotNull
+    @PositiveOrZero
     Long quantity
 ) {}
