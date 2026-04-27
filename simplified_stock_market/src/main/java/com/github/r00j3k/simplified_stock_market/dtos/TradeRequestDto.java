cc4ce1fdd7a2c6a2b811d9eb377e4206e8307e49
r00j3K <adrianr8@o2.pl>
2026-04-27 14:59:53 +0200

    Change validation of TransactionType enum

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/TradeRequestDto.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/TradeRequestDto.java
@@ -1,11 +1,9 @@
 package com.github.r00j3k.simplified_stock_market.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.validation.constraints.Pattern;
+import com.github.r00j3k.simplified_stock_market.enums.TransactionType;
 
 public record TradeRequestDto(
-    @Pattern(regexp = "(?i)BUY|SELL", message = "Type must be BUY or SELL case insensitive")
     @JsonProperty("type")
-    String transactionType
+    TransactionType transactionType
 ) {}
--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/enums/TransactionType.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/enums/TransactionType.java
@@ -1,6 +1,12 @@
 package com.github.r00j3k.simplified_stock_market.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TransactionType {
-    BUY,
-    SELL
+    BUY, SELL;
+
+    @JsonCreator
+    public static TransactionType from(String type) {
+        return TransactionType.valueOf(type.toUpperCase());
+    }
 }
