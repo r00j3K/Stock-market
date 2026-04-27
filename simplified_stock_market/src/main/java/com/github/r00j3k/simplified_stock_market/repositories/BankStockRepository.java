e28aa40e8f27b58174e261c4b988de53f56f7191
r00j3K <adrianr8@o2.pl>
2026-04-27 16:06:40 +0200

    Add custom query for repository method

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/BankStockRepository.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/repositories/BankStockRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.github.r00j3k.simplified_stock_market.entities.BankStock;
 
@@ -11,5 +13,6 @@ import jakarta.persistence.LockModeType;
 
 public interface BankStockRepository extends JpaRepository<BankStock, String> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<BankStock> findByIdForUpdate(String stockName);
+    @Query("SELECT b FROM BankStock b WHERE b.stockName = :stockName")
+    Optional<BankStock> findByIdForUpdate(@Param("stockName") String stockName);
 }
