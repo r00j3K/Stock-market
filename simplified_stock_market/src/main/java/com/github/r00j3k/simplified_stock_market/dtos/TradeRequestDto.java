2fa7ab9b6d70f760832d499e35a4982bd8d06b9f
r00j3K <adrianr8@o2.pl>
2026-04-27 16:06:13 +0200

    Add NotNull validation for field in TradeRequestDto

--- simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/TradeRequestDto.java
+++ simplified_stock_market/src/main/java/com/github/r00j3k/simplified_stock_market/dtos/TradeRequestDto.java
@@ -3,7 +3,10 @@ package com.github.r00j3k.simplified_stock_market.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.r00j3k.simplified_stock_market.enums.TransactionType;
 
+import jakarta.validation.constraints.NotNull;
+
 public record TradeRequestDto(
     @JsonProperty("type")
+    @NotNull
     TransactionType transactionType
 ) {}
