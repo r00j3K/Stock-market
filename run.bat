4af6cc109ba20b704f2f06b0986bb784dcbe811a
r00j3K <adrianr8@o2.pl>
2026-05-01 14:33:45 +0200

    Add validation for port value

--- run.bat
+++ run.bat
@@ -4,6 +4,16 @@ setlocal
 set APP_PORT=%1
 if "%APP_PORT%"=="" set APP_PORT=8080
 
+REM Check if APP_PORT value is a number within the valid TCP range: 1-65535
+echo %APP_PORT%| findstr /r "^[0-9][0-9]*$" >nul
+if errorlevel 1 (
+    echo Invalid port number. Setting to 8080...
+    set APP_PORT=8080
+) else (
+    if %APP_PORT% LSS 1 set APP_PORT=8080
+    if %APP_PORT% GTR 65535 set APP_PORT=8080
+)
+
 echo -------------------------------------------------------
 echo Running...
 echo To access service use provided URL: http://localhost:%APP_PORT%
--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 export APP_PORT=${1:-8080}
 
+# Check if APP_PORT value is a number within the valid TCP range: 1-65535
+if ! [[ "$APP_PORT" =~ ^[0-9]+$ ]] || [[ "$APP_PORT" -lt 1 ]] || [[ "$APP_PORT" -gt 65535 ]]; then
+    echo "Invalid port number. Setting to default value 8080..."
+    APP_PORT=8080
+fi
+
 echo "-------------------------------------------------------"
 echo "Running..."
 echo "To access service use provided URL: http://localhost:$APP_PORT"
