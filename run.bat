9acc11877022c86535e09cdd6f3b564cfde431d3
r00j3K <adrianr8@o2.pl>
2026-05-01 15:30:57 +0200

    Refactor run.bat

--- run.bat
+++ run.bat
@@ -4,14 +4,20 @@ setlocal
 set APP_PORT=%1
 if "%APP_PORT%"=="" set APP_PORT=8080
 
-REM Check if APP_PORT value is a number within the valid TCP range: 1-65535
-echo %APP_PORT%| findstr /r "^[0-9][0-9]*$" >nul
-if errorlevel 1 (
+set INVALID=0
+
+@REM Check if APP_PORT value is a number 
+echo %APP_PORT%| findstr /r "^[0-9][0-9]*$" >nul || set INVALID=1
+
+@REM Check if APP_PORT is within the valid TCP range: 1-65535
+if %INVALID%==0 (
+    if %APP_PORT% LSS 1 set INVALID=1
+    if %APP_PORT% GTR 65535 set INVALID=1
+)
+
+if %INVALID%==1 (
     echo Invalid port number. Setting to 8080...
     set APP_PORT=8080
-) else (
-    if %APP_PORT% LSS 1 set APP_PORT=8080
-    if %APP_PORT% GTR 65535 set APP_PORT=8080
 )
 
 echo -------------------------------------------------------
